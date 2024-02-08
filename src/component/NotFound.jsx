ee668e10806e949f6a242b4ea55f9a9716af5d5e
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2024-02-08 08:37:35 +0200

    Your commit message here

--- src/component/NotFound.jsx
+++ src/component/NotFound.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <div>notFound</div>
-  )
+    <>
+      <div>
+        <div className="d-flex align-items-center justify-content-center py-5">
+          <div className="text-center row">
+            <div className=" col-md-6">
+              <h1 className=" text-danger">404</h1>
+            </div>
+            <div className=" col-md-6 mt-3">
+              <p className="fs-3">
+                {" "}
+                <span className="text-danger">Opps!</span> Page not found.
+              </p>
+              <p className="lead">The page you’re looking for doesn’t exist.</p>
+              <Link to="" className="btn btn-primary">
+                Go Home
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
